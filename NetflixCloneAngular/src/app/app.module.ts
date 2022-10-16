20047cffb25c9c257531d09de4f6013840cce50b
alejo fontao <alefonta@hotmail.com.ar>
2022-10-16 12:04:09 -0300

    Nav y estilos

--- NetflixCloneAngular/src/app/app.module.ts
+++ NetflixCloneAngular/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContentCardComponent } from './core/content-card/content-card.component';
+import { SliderComponent } from './core/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

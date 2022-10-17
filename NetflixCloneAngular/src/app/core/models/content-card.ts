48b0b4fcb14ce0b5317435831d2385e647b772ad
alejo fontao <alefonta@hotmail.com.ar>
2022-10-17 15:58:37 -0300

    Nuevas cards

--- NetflixCloneAngular/src/app/core/models/content-card.ts
+++ NetflixCloneAngular/src/app/core/models/content-card.ts
@@ -7,6 +7,13 @@ export interface Slider {
 }
 
 export const contentCards1: ContentCard[] = [
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWK61a9w1GL6rqN9weiOavtaQZpNrRgMAWwJUji10nxDwLnr5Unvb8zSaNVstiBF5PLWGkOR-axwypzSPc2KzfKaSjg0-OiubtTeuVyGbybylJyh3zHFKxT7JGPfA_Sv4Jaq.jpg?r=c3c' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXeeKbX0nIqkLb5xcLTLpW9Y6nahUXdJ38r2X82t2XMyc75g1bHIKGh-u2bWpC8KV_qd0G7-nPulOeK2g456ijEft_Qkncdf7S5_Y3m5X2VPe0dBTvw8oivT395AAYro2yRJ.jpg?r=cba' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWfd_wVcG1RCNLvADPocoeNA4mOyZniNN4dpnqRyFqN_QYT5JxIFBZb3AlDVuJJVWbF8FKVZTxTzop3Ni-PohyWTKQ_5T9onM6wiEIjyjUJUBF0GlaejyS4T_kI1ISckM9L1.jpg?r=5a4' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZERXhg_PALj0H6xga6SsKr7wRRNouxjbnArPh_eTNv5vUSFp7QgfrngZgUTyJhtGWZPT7wqTB5mUBXu0TVo3FhTkIBasaW8suu2WQK1xNuKDxH-TZJMTDd_hCx5ai-wWPbL.jpg?r=2f0' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQ2vGA186kEI7CJ3Lnm4QZ_1a0HTgLuOm0hC4poJSf3TwjNItzHnHpDDDcOeZ3Dk5bGSUuycE_i9QPYfv8npO00vv1CCYASsH2AmLSKjurObYAcCopWqvMPQmh0htzfxsNpv.jpg?r=f86' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUSnyZDs93YcMqWVZn0B1HgW31Tht7htMi_pghZMu_-7dhCs1V1u4hKcJJ96jqYCpzi8uqQgj4na6kVM8GKm5tjJKf71iL885c0p5BlkVagfnVyYW9RjtjzieUukRimQc5w43dWShMk1Ipfv0F81ioNdGlDUlk5vPKEmmQm9WTp9RpqSwWuToLkB0yn0htg.jpg?r=8d6' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUl4xF0gXIfTjo5yMX9cSk46sOdCJMM9Fc6fTPEM8aNTJaaNpLBkLSvHfAiVlFExUTRRzjwMDZ2Adsl6u0pKEEObwSL-9K2RZVpFWi4M1nwr43ymlcTh_QQI7E9QFmSiV88GdJlhxAvI_vHrgDrnpBN2IjIjxzqWriKbm-0xMUA5jns6IoU8AL1RT6dNjX8.jpg?r=1b6' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXiKPBZGG7rIUqB_Aeqx9dU-jtQL-JFLqvixpuwf532aGQbxHxdL3O4OVN5BV23W4Yt5BP_2kp0QvATU4wx1aul_gGhs6KxIih9vide_Wf5NRWfFwIshOwB7DLIX1NOqzra8.jpg?r=354' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQOQdHb1z1amHfzBqcqHovwDPP5yd-BPv0OAlYPsYRR3_W-9570JDiUJ2u2VYTjxPLFEkqZZaaZ4U1AzZb2m6SyjQNjNhuR2ctStvCIcxtWkSPnsGpG-biBFP5ME9AKFmz1f.jpg?r=2ee' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABew2yOimNrplXwv8mVSuReqWsDNpIxO8DBrY_0rntj3LGJdZXz0wZKI5Z3H7ahKJyKtBADqtR0S6AF_sSSjyzc_hxeqWEg-haG_VqI_Zc1yhVavXR0rfuI-e8rd-N1pP5yTA.jpg?r=37e' },
@@ -20,10 +27,15 @@ export const contentCards1: ContentCard[] = [
 
 
 export const contentCards2: ContentCard[] = [
+    { img: 'https://occ-0-3791-185.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZw9zrpS_q31_3vrALivPm-AcU70SuDHf-IV_R9XWy9cn2_bgeLWEY7ve1OSHFe86MslAgXy_sA9K06dYX2g55xAm5DG_XwxJhc.webp?r=f04' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABTJ__aFxZPXh3Cw5t7u82sQNt1TJLK7Oguxhe0mnTQO4WDRK2A0f-Nz9oNPq-bN9PYm2t6RWz3GAEVJ453_wFiwF8gVUHA031gk.webp?r=73d' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABYuKjjKMzFh6RN2q7ml5nJDrNguQzImqCdD7tTYKKUXYnliwYVjasxzPERdiwpcDhL8zqUyaRHnRhYWtajxpwaRLf1FUTtHR7CGGFkn028rZF0CCpGfBAYA-e6H0DdaVUrdO.jpg?r=393' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQfr25pN4DmY4YWQDDCrhFpgPd7DTRxjPk1sGrtccWxMJvtar4OVk562deY1wxMEKd9fU2RCuoxlrr_Q0vyUmpxBys77fraqjRQ.webp?r=6d8' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfpuZ1gUZtOn8pNwZM2tbAylpbXXplsZJDDsmEmWb_ylt1caGyf24zLFu1roTJ4qUsNiuSdPk3wy28lldIfICcWE2OQLg-LPkFby85U3zcxHtfApyStnGDUV_3aszZTJB_4Y.jpg?r=58a' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfuoGPAFqsWzJbZE1ZigskisFRlNPMUlHdzzywu6GBwv_wU5BnRVXzTsTpTBtPV5MX-29tuuWbqetfX6QqtHnmisujQtJG0Xbx8rH9ih1fxCPSEcG4EatX62gyQfmR-QhelF.jpg?r=b0f' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXiKPBZGG7rIUqB_Aeqx9dU-jtQL-JFLqvixpuwf532aGQbxHxdL3O4OVN5BV23W4Yt5BP_2kp0QvATU4wx1aul_gGhs6KxIih9vide_Wf5NRWfFwIshOwB7DLIX1NOqzra8.jpg?r=354' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQOQdHb1z1amHfzBqcqHovwDPP5yd-BPv0OAlYPsYRR3_W-9570JDiUJ2u2VYTjxPLFEkqZZaaZ4U1AzZb2m6SyjQNjNhuR2ctStvCIcxtWkSPnsGpG-biBFP5ME9AKFmz1f.jpg?r=2ee' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQ2vGA186kEI7CJ3Lnm4QZ_1a0HTgLuOm0hC4poJSf3TwjNItzHnHpDDDcOeZ3Dk5bGSUuycE_i9QPYfv8npO00vv1CCYASsH2AmLSKjurObYAcCopWqvMPQmh0htzfxsNpv.jpg?r=f86' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABew2yOimNrplXwv8mVSuReqWsDNpIxO8DBrY_0rntj3LGJdZXz0wZKI5Z3H7ahKJyKtBADqtR0S6AF_sSSjyzc_hxeqWEg-haG_VqI_Zc1yhVavXR0rfuI-e8rd-N1pP5yTA.jpg?r=37e' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXiKPBZGG7rIUqB_Aeqx9dU-jtQL-JFLqvixpuwf532aGQbxHxdL3O4OVN5BV23W4Yt5BP_2kp0QvATU4wx1aul_gGhs6KxIih9vide_Wf5NRWfFwIshOwB7DLIX1NOqzra8.jpg?r=354' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQOQdHb1z1amHfzBqcqHovwDPP5yd-BPv0OAlYPsYRR3_W-9570JDiUJ2u2VYTjxPLFEkqZZaaZ4U1AzZb2m6SyjQNjNhuR2ctStvCIcxtWkSPnsGpG-biBFP5ME9AKFmz1f.jpg?r=2ee' },
@@ -36,12 +48,10 @@ export const contentCards3: ContentCard[] = [
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfpuZ1gUZtOn8pNwZM2tbAylpbXXplsZJDDsmEmWb_ylt1caGyf24zLFu1roTJ4qUsNiuSdPk3wy28lldIfICcWE2OQLg-LPkFby85U3zcxHtfApyStnGDUV_3aszZTJB_4Y.jpg?r=58a' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfuoGPAFqsWzJbZE1ZigskisFRlNPMUlHdzzywu6GBwv_wU5BnRVXzTsTpTBtPV5MX-29tuuWbqetfX6QqtHnmisujQtJG0Xbx8rH9ih1fxCPSEcG4EatX62gyQfmR-QhelF.jpg?r=b0f' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXiKPBZGG7rIUqB_Aeqx9dU-jtQL-JFLqvixpuwf532aGQbxHxdL3O4OVN5BV23W4Yt5BP_2kp0QvATU4wx1aul_gGhs6KxIih9vide_Wf5NRWfFwIshOwB7DLIX1NOqzra8.jpg?r=354' },
-    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXiKPBZGG7rIUqB_Aeqx9dU-jtQL-JFLqvixpuwf532aGQbxHxdL3O4OVN5BV23W4Yt5BP_2kp0QvATU4wx1aul_gGhs6KxIih9vide_Wf5NRWfFwIshOwB7DLIX1NOqzra8.jpg?r=354' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQOQdHb1z1amHfzBqcqHovwDPP5yd-BPv0OAlYPsYRR3_W-9570JDiUJ2u2VYTjxPLFEkqZZaaZ4U1AzZb2m6SyjQNjNhuR2ctStvCIcxtWkSPnsGpG-biBFP5ME9AKFmz1f.jpg?r=2ee' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABew2yOimNrplXwv8mVSuReqWsDNpIxO8DBrY_0rntj3LGJdZXz0wZKI5Z3H7ahKJyKtBADqtR0S6AF_sSSjyzc_hxeqWEg-haG_VqI_Zc1yhVavXR0rfuI-e8rd-N1pP5yTA.jpg?r=37e' },
     { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABThf2j5jL4PUzeNID0a0IQG_VX6cbZPbpOGrkYKIyd5K4hRMFFYgN2UcCzXZ-ClrjXARduvJW1gr67Akb1tPgFKM_k3SHt-0nvbV2bs8xnoogqjcF1hvgzWhRDUH8BgIbWe5.jpg?r=3cd' },
-    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQOQdHb1z1amHfzBqcqHovwDPP5yd-BPv0OAlYPsYRR3_W-9570JDiUJ2u2VYTjxPLFEkqZZaaZ4U1AzZb2m6SyjQNjNhuR2ctStvCIcxtWkSPnsGpG-biBFP5ME9AKFmz1f.jpg?r=2ee' },
-    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABew2yOimNrplXwv8mVSuReqWsDNpIxO8DBrY_0rntj3LGJdZXz0wZKI5Z3H7ahKJyKtBADqtR0S6AF_sSSjyzc_hxeqWEg-haG_VqI_Zc1yhVavXR0rfuI-e8rd-N1pP5yTA.jpg?r=37e' },
+    { img: 'https://occ-0-3791-114.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQfr25pN4DmY4YWQDDCrhFpgPd7DTRxjPk1sGrtccWxMJvtar4OVk562deY1wxMEKd9fU2RCuoxlrr_Q0vyUmpxBys77fraqjRQ.webp?r=6d8' },
 ];
 
 export const slider1: Slider =

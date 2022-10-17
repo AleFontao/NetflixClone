71cc6fc2febe2d77683d26b56214c53f7765f8f3
alejo fontao <alefonta@hotmail.com.ar>
2022-10-17 10:53:51 -0300

    Carrusel

--- NetflixCloneAngular/src/app/core/slider/slider.component.ts
+++ NetflixCloneAngular/src/app/core/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { cards, ContentCard } from '../models/content-card';
 
 @Component({
   selector: 'app-slider',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+  cards!: ContentCard[];
+  sliderContent: ContentCard[] = cards;
+  index: number = 1;
 
+  slidesContainer!: HTMLElement | null;
   constructor() { }
 
   ngOnInit() {
+    this.cards = cards;
+    this.slidesContainer = document.getElementById("slides-container");
   }
+  right() {
+    this.index++;
+    console.log(this.index + " " + this.cards.length % this.index);
+    if (this.cards.length % this.index != 0) {
+      this.resetIndex();
+    }
+
+    const slideWidth = this.slidesContainer!.clientWidth;
+    this.slidesContainer!.scrollLeft += slideWidth;
+  }
+
+  left() {
+    if (this.index > 1) {
+      this.index--;
+    }
+    console.log(this.index + " " + this.cards.length % this.index);
+    const slideWidth = this.slidesContainer!.clientWidth;
+    this.slidesContainer!.scrollLeft -= slideWidth;
+  }
+
+  resetIndex() {
+    this.index = 1;
+    this.sliderContent.forEach(element => this.cards.push(element))
+  }
+
+
 
 }
